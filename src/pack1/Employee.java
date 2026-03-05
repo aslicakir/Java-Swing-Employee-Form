3960c172d30d02b07b9001ccb49672922abf3edd
aslicakir <aslicakirogluu1@gmail.com>
2026-03-06 00:51:46 +0300

    add file.

---
+++ src/pack1/Employee.java
@@ -0,0 +1,53 @@
+package pack1;
+
+import java.util.ArrayList;
+
+public class Employee {
+
+	String name, surname, city, gender;
+	int age;
+	
+	ArrayList<String> languages;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
